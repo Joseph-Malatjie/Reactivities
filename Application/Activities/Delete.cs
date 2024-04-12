833ab0c4f865958ab3c7e2f484a325af50399947
Joseph Malatjie <mjmalatjie95@gmail.com>
2024-04-12 20:50:28 +0200

    End of section 4

---
+++ Application/Activities/Delete.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace Application.Activities;
+
+public class Delete
+{
+    public class Command : IRequest
+    {
+        public Guid Id { get; set; }
+    }
+
+    public class Handler : IRequestHandler<Command>
+    {
+        private readonly DataContext _dataContext;
+        private readonly ILogger<Delete> _logger;
+
+        public Handler(DataContext dataContext, ILogger<Delete> logger)
+        {
+            _dataContext = dataContext;
+            _logger = logger;
+        }
+        
+        public async Task Handle(Command request, CancellationToken cancellationToken)
+        {
+            var activity = await _dataContext.Activities.FindAsync(request.Id);
+            
+            if (activity == null)
+            {
+                throw new Exception("Activity not found");
+            }
+
+            _dataContext.Remove(activity);
+            await _dataContext.SaveChangesAsync();
+        }
+    }
+}
