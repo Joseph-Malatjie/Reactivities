6a59a267b2b1eefc99a9eee5d5929b80c3f2d8d1
Joseph Malatjie <mjmalatjie95@gmail.com>
2024-04-14 14:03:41 +0200

    End of Section 6

--- social-app/src/features/activities/form/ActivityForm.tsx
+++ social-app/src/features/activities/form/ActivityForm.tsx
@@ -6,13 +6,15 @@ interface Props {
      activity: Activity | undefined;
      closeForm: () => void;
      createOrEdit: (activity: Activity) => void;
+     submitting: boolean;
 }
 
 export default function ActivityForm(
     {
         activity: selectedActivity,
         closeForm,
-        createOrEdit
+        createOrEdit,
+        submitting
     }: Props) {
     
     const initialState = selectedActivity ?? {
@@ -45,7 +47,7 @@ export default function ActivityForm(
                 <Form.Input type='date' placeholder='Date' value={activity.date} name='date' onChange={handleInputChange}/>
                 <Form.Input placeholder='City' value={activity.city} name='city' onChange={handleInputChange}/>
                 <Form.Input placeholder='Venue' value={activity.venue} name='venue' onChange={handleInputChange}/>
-                <Button floated='right' positive type='submit' content='Submit'/>
+                <Button loading={submitting} floated='right' positive type='submit' content='Submit'/>
                 <Button onClick={closeForm} floated='right' type='button' color='orange' content='Cancel'/>
             </Form>
         </Segment>
