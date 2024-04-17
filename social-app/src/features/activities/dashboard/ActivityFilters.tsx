07e53ff7b15e84163ad5d4c26e15efc96cbafbec
Joseph Malatjie <mjmalatjie95@gmail.com>
2024-04-17 16:44:23 +0200

    End of section 9

---
+++ social-app/src/features/activities/dashboard/ActivityFilters.tsx
@@ -0,0 +1,33 @@
+﻿import { observer } from "mobx-react-lite";
+import { useStore } from "../../../app/stores/Store";
+import {Header, Menu} from "semantic-ui-react";
+import {Calendar} from "react-calendar";
+
+export default observer(function ActivityFilters() {
+    const {activityStore} = useStore();
+    const {/*predicate, setPredicate*/} = activityStore;
+    return (
+        <>
+            <Menu vertical size='large' style={{width: '100%', marginTop:25 }}>
+                <Header icon='filter' attached color='teal' content='Filters'/>
+                <Menu.Item
+                    content='All Activities'
+                    //active={predicate.has('all')}
+                    //onClick={() => setPredicate('all', 'true')}
+                />
+                <Menu.Item
+                    content="I'm going"
+                    //active={predicate.has('isGoing')}
+                    //onClick={() => setPredicate('isGoing', 'true')}
+                />
+                <Menu.Item
+                    content="I'm hosting"
+                    //active={predicate.has('isHost')}
+                    //onClick={() => setPredicate('isHost', 'true')}
+                />
+            </Menu>
+            <Header /*icon={'calendar'} attached color='teal' content={'Select Date'}*//>
+            <Calendar/>
+        </>
+    )
+})
